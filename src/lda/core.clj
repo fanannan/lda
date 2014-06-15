fa505f4747e1df0515ec74e58de51604856879b1
fanannan <fanannan@users.noreply.github.com>
2014-06-15 12:04:56 +0900

    Update core.clj

--- src/lda/core.clj
+++ src/lda/core.clj
@@ -4,8 +4,7 @@
 
 (defn read-csv [input-file-path] ; read a data file and returns [[id, word, word-count],...]
   (with-open [input-file (io/reader input-file-path)]
-    (doall (map (fn[[id word c]][id word (Integer/valueOf c)])
-                (rest (csv/read-csv input-file))))))
+    (doall (map (fn[[id word c]][id word (Integer/valueOf c)])(rest (csv/read-csv input-file))))))
 
 (defn unique-words [data]
   (count (set (map (fn[[id word count]] word) data))))
@@ -18,13 +17,13 @@
        (map (fn[[[_ _ count] _]] count))(apply +)))
 
 (defn update-probs [topic id word alpha beta v topiced-data]
-  (let [n_td (count-words (fn[t d w](and (= topic t)(= id d))) topiced-data)
-        n_wt (count-words (fn[t d w](and (= topic t)(= word w))) topiced-data)
+  (let [n_td (count-words (fn[t d w](and (= topic t)(= id d))) topiced-data),
+        n_wt (count-words (fn[t d w](and (= topic t)(= word w))) topiced-data),
         n_t  (count-words (fn[t d w](= topic t)) topiced-data)]
     (+ alpha (* n_td (+ beta n_wt) (/ 1 (+ (* beta v) n_t))))))
 
 (defn gibbs [[id word _] alpha beta v k topiced-data] ;collapsed gibbs sampling
-  (let [raw-probs (map #(update-probs % id word alpha beta v topiced-data) (range k))
+  (let [raw-probs (map #(update-probs % id word alpha beta v topiced-data) (range k)),
         cum-probs (reductions + raw-probs)]
     (count (filter #(< % (rand)) (map #(/ % (last cum-probs)) cum-probs)))))
 
@@ -37,10 +36,10 @@
        (map (fn[[datum freq]] [datum (map (fn[i](float (/ (count (filter #(= % i) freq)) (count samples)))) (range k))]))))
 
 (defn lda [alpha beta k iteration input-file-path] ; k : number of topics
-  (let [data (read-csv input-file-path)
-        v (unique-words data)
+  (let [data (read-csv input-file-path),
+        v (unique-words data),
         samples (rest (reductions (fn[dt i](reassign-topic alpha beta v k dt i))(assign-topic data k)(range iteration)))]
     (probabilities k samples)))
 
-(defn -main []
+(defn -main [& args]
   (println (lda (/ 50 5) 0.1 5 50 "./resources/in1_small.csv")))
